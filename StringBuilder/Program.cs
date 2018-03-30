8061e8745ae7bd6a2fdc4b55fc7e12b1345fa67c
JohnMorris4 <johnmorris@morrisje.com>
2018-03-30 04:32:33 -0400

    Finished section on using StringBuilder

--- StringBuilder/Program.cs
+++ StringBuilder/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StringBuilder;
+using System.Text;
 
 namespace StringBuilder
 {
@@ -10,6 +12,15 @@ namespace StringBuilder
     {
         static void Main(string[] args)
         {
+            var builder = new System.Text.StringBuilder();
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+            builder.Replace('-', '+');
+            Console.WriteLine(builder);
+            Console.Read();
         }
     }
 }
